de7e862f67dfc52408b09122ef4c5f8bbd0adffd
Nic Martin <nicholas.martin@marketdojo.com>
2022-03-05 18:01:33 +0000

    Debug
    debug to figure out why reports not working

--- app/mailboxes/dmarc_reports_mailbox.rb
+++ app/mailboxes/dmarc_reports_mailbox.rb
@@ -10,16 +10,22 @@ class DmarcReportsMailbox < ApplicationMailbox
   private
 
   def create_records_from_attachment(mail)
+    Rails.logger.info "Processing attachments"
     mail.attachments.each do |attachment|
+      debugger
+      Rails.logger.info "Attachment: #{attachment.filename} "
       # TODO: DRY
       xml = ParseAttachment.new(attachment).content
+      Rails.logger.info "XML: #{xml}"
       dmarc = ParseDmarc.new(xml)
       pp = PolicyPublished.find_or_create_by(dmarc.policy_published)
+      Rails.logger.info "Policy: #{pp.id}"
       report = Report.create(dmarc.metadata.merge(policy_published: pp))
-
+      Rails.logger.info "Report: #{report.id}"
       dmarc.records.each do |row|
         report.records << Record.create(row)
       end
+      Rails.logger.info "Records: #{dmarc.records.length}"
     end
   end
 end
