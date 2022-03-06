6b4c227777d6b49fea006ea29b43c6fb3e9abca2
Nic Martin <nicholas.martin@marketdojo.com>
2022-03-06 17:18:36 +0000

    Metrics as components not partials
    switch to view components for metrics

--- app/components/metric_component.rb
+++ app/components/metric_component.rb
@@ -6,5 +6,4 @@ class MetricComponent < ViewComponent::Base
     @icon = icon
     @value = value
   end
-
 end
--- test/components/metric_component_test.rb
+++ test/components/metric_component_test.rb
@@ -2,7 +2,17 @@ require 'test_helper'
 
 class MetricComponentTest < ViewComponent::TestCase
   def test_component_renders_title
-    render_inline(CardComponent.new(title: 'Widget!')) { 'Hello World!' }
+    render_inline(MetricComponent.new(title: 'Widget!', icon: 'fa-calendar', value: '999'))
     assert_selector("h2[class='font-bold uppercase text-gray-600']", text: 'Widget!')
+  end
+
+  def test_component_renders_icon
+    render_inline(MetricComponent.new(title: 'Widget!', icon: 'fa-calendar', value: '999'))
+    assert_selector('i', class: /fa\-calendar/)
+  end
+
+  def test_component_renders_value
+    render_inline(MetricComponent.new(title: 'Widget!', icon: 'fa-calendar', value: '999'))
+    assert_selector('p', text: '999')
   end
 end
