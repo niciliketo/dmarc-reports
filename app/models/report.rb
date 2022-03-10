f5ac991fce4b3380ad32f03e5d39422e1f30a95c
Nic Martin <nicholas.martin@marketdojo.com>
2022-03-10 22:24:30 +0000

    Some aggregation
    no tests yet!

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,29 @@
 class Report < ApplicationRecord
   has_many :records, dependent: :destroy
   belongs_to :policy_published
+
+  def total_records
+    record_summary.values.sum
+  end
+
+  def spf_fail_records
+    record_summary[%w[fail pass]].to_i + record_summary[%w[fail fail]].to_i
+  end
+
+  def dkim_fail_records
+    record_summary[%w[pass fail]].to_i + record_summary[%w[fail fail]].to_i
+  end
+
+  def record_total
+    record_summary.values.sum
+  end
+
+  private
+
+  # Get a summary of the records
+  # grouped by :spf and dkim
+  # we will use this to return summary metrics
+  def record_summary
+    records.group(:spf, :dkim).count
+  end
 end
